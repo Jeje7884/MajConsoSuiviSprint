8d389b253079fc58a5633d024245054e669c47ce
GG BRUGAL <jerome.brugal@gmail.com>
2023-08-12 00:20:00 +0200

    Débug confgi

---
+++ MajConsoSuiviSprint.Cli/Model/WebTTTInfoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MajConsoSuiviSprint.Cli.Model
+{
+    public class WebTTTInfoModel
+    {
+
+        public int NbreSprintAPrendreEnCompte { get; set; } = 0;
+        public string FolderName { get; set; } = default!;
+        public string FileName { get; set; } = default!;
+        public string SheetName { get; set; } = default!;
+        public IReadOnlyCollection<HeadersWebTTT> Headers { get; set; } = new List<HeadersWebTTT>();
+
+
+    }
+}
